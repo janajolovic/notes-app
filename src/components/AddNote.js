e8054f2433e575ac4667a16c7be9cb89b8785459
Jana Jolovic <janajolovic04@gmail.com>
2021-09-21 13:12:20 +0200

    delete a note

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,14 +1,35 @@
-const AddNote = () => {
+import { useState } from "react";
+
+const AddNote = ({ handleAddNote }) => {
+    const [ noteText, setNoteText ] = useState("")
+
+    const characterLimit = 200;
+
+    const handleChange = (event) => {
+        if (characterLimit - event.target.value.length >= 0) {
+            setNoteText(event.target.value)
+        }
+    }
+
+    const handleSaveClick = () => {
+        if(noteText.trim().length > 0) {
+            handleAddNote(noteText)
+            setNoteText('')
+        }
+    }
+
     return (
         <div className="note new">
             <textarea 
                 rows="8" 
                 cols="10" 
-                placeholder="Type to add a note...">
+                placeholder="Type to add a note..."
+                value = {noteText}
+                onChange={handleChange}>
             </textarea>
             <div className="note-footer">
-                <small>200 remaining</small>
-                <button className="save">Save</button>
+                <small>{characterLimit - noteText.length} remaining</small>
+                <button className="save" onClick={handleSaveClick}>Save</button>
             </div>
         </div>
     )
