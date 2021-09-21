74644766b6fca65a88b64c6ff22f304cc5635a00
Jana Jolovic <janajolovic04@gmail.com>
2021-09-21 14:16:39 +0200

    saving notes to local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import NotesList from "./components/notesList";
 import Search from "./Search";
@@ -26,6 +26,19 @@ const App = () => {
   const [ searchText, setSearchText ] = useState("");
 
   const [ darkMode, setDarkMode ] = useState(false);
+
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"))
+
+    if (savedNotes) {
+      setNotes(savedNotes)
+    }
+  }, [])
+  
+  useEffect(() => {
+    localStorage.setItem("react-notes-app-data", JSON.stringify(notes))
+  }, [notes])
 
   const addNote = (text) => {
     const date = new Date();
